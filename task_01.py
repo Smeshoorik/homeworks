a20860f801d8f7f7ddb0a3d9dca5da1543b201df
Alexandra Lesnaya <116189339+Smeshoorik@users.noreply.github.com>
2022-11-27 00:50:10 +0300

    Update task_01.py
    added answer to a task

--- task_01.py
+++ task_01.py
@@ -2,6 +2,10 @@
 # Есть строка с перечислением песен
 
 my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+songs_list = my_favorite_songs.split(', ')
+songs = songs_list[0], songs_list[-1], songs_list[1], songs_list[-2]
+
+print(*songs, sep="\n")
 
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
-# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться
